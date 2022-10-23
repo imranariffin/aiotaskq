6d6c261d9d94c59c52d1ff25bb42a2f4091696da
Imran Ariffin <ariffin.imran@gmail.com>
2022-10-23 01:12:26 -0400

    Debug

--- src/aiotaskq/tests/apps/simple_app/app.py
+++ src/aiotaskq/tests/apps/simple_app/app.py
@@ -1,12 +1,12 @@
+import asyncio
+
 from .tasks import join
 
 
 if __name__ == "__main__":  # pragma: no cover
-    from asyncio import get_event_loop
 
     async def main():
         ret = await join.apply_async(["Hello", "World"], delimiter=" ")
         print(ret)
 
-    loop = get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
