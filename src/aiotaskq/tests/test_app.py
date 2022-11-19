5bb9b4c231856d8f04d8fe2e9454a526f73c4d9d
Imran Ariffin <ariffin.imran@gmail.com>
2022-11-19 10:07:32 -0500

    Fixup

--- src/aiotaskq/tests/test_app.py
+++ src/aiotaskq/tests/test_app.py
@@ -13,38 +13,42 @@ from simple_app.aiotaskq import app as simple_app
     [
         (
             # Case 1: Tasks are defined inside a module without using explicit Aiotaskq
-            # instance, and we're instantiating Aiotaskq using the module path.
+            # instance, and
             "simple_app.aiotaskq",
+            # We should instantiate Aiotaskq using the module path.
             simple_app,
         ),
         # (
-        #     # Case 2: Tasks are defined using an explicit Aiotaskq instance, and we're
-        #     # instantiating Aiotaskq from the import path to the instance.
+        #     # Case 2: Tasks are defined using an explicit Aiotaskq instance, and 
         #     "aiotaskq.tests.apps.simple_app_encapsulated",
+        #     # We should instantiate Aiotaskq from the import path to the instance.
         #     aiotaskq.tests.apps.simple_app_encapsulated.aiotaskq.app,
         # ),
         # (
         #     # Case 3: Tasks are defined using an explicit Aiotaskq instance in a default
-        #     # file name ("aiotaskq.py"), and we're instantiating Aiotaskq from the import
-        #     # path to the instance using the ":" pattern (this pattern is useful to
-        #     # differentiate between a module and an object).
+        #     # file name ("aiotaskq.py"), and
         #     "aiotaskq.tests.apps.simple_app_encapsulated.aiotaskq:app",
+        #     # We should instantiate Aiotaskq from the import path to the instance
+        #     # using the ":" pattern (this pattern is useful to differentiate between
+        #     # a module and an object).
         #     aiotaskq.tests.apps.simple_app_encapsulated.aiotaskq.app,
         # ),
         # (
-        #     # Case 4: Tasks are defined using an explicit Aiotaskq instance in a non-default
-        #     # file name ("some_module_name.py"), and we're instantiating Aiotaskq from the
-        #     # import path to the instance using the ":" pattern (this pattern is useful to
-        #     # differentiate between a module and an object)".
+        #     # Case 4: Tasks are defined using an explicit Aiotaskq instance in
+        #     # a non-default file name ("some_module_name.py"), and
         #     "aiotaskq.tests.apps.simple_app_encapsulated_2.some_file_name:some_app",
+        #     # We should instantiate Aiotaskq from the import path to the instance
+        #     # using the ":" pattern (this pattern is useful to differentiate between
+        #     # a module and an object)".
         #     aiotaskq.tests.apps.simple_app_encapsulated_2.some_file_name.some_app,
         # ),
         # (
         #     # Case 5: Tasks are defined using an explicit Aiotaskq instance in a non-default
-        #     # file name ("some_module_name.py"), and we're instantiating Aiotaskq from the
-        #     # import path to the instance using the ":" pattern (this pattern is useful to
-        #     # differentiate between a module and an object)".
+        #     # file name ("some_module_name.py"), and
         #     "aiotaskq.tests.apps.simple_app_encapsulated_2.some_file_name",
+        #     # We should instantiate Aiotaskq from the import path to the instance
+        #     # using the ":" pattern (this pattern is useful to differentiate between
+        #     # a module and an object)".
         #     aiotaskq.tests.apps.simple_app_encapsulated_2.some_file_name.some_app,
         # ),
     ],
@@ -56,5 +60,5 @@ def test_valid_app_import_path(valid_import_path: str, app_expected: ModuleType)
     app_actual = Aiotaskq.from_import_path(valid_import_path)
 
     # Then the instantiated Aiotaskq object should be loaded with the tasks
-    assert isinstance(app_actual.task_map["add"], Task)
+    assert isinstance(app_actual.add, Task)
     assert app_expected.add == app_actual.task_map["add"] == app_actual.add  # type: ignore
--- test.sh
+++ test.sh
@@ -1,16 +1,23 @@
-PROJECT_DIR=$PWD envsubst < ./sample_apps/simple_app/pyproject.template.toml > ./sample_apps/simple_app/pyproject.toml
+echo "Upgrade pip"
+pip install --quiet --upgrade pip
 
+echo "Install sample projects"
+PROJECT_DIR=$PWD envsubst < ./sample_apps/simple_app/pyproject.template.toml > ./sample_apps/simple_app/pyproject.toml
+pip install --quiet -e ./sample_apps/simple_app/
+
+echo "Erase previous coverage files"
 coverage erase
 
+echo "Run tests"
 if [ -z $1 ];
 then
     coverage run -m pytest -v
 else
     coverage run -m pytest -vvv -k $1 -s
 fi
-
 failed=$?
 
+echo "Combine coverage files"
 coverage combine
 
 exit $failed
