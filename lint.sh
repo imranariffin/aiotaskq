7274fcd205e4eb2a2ef3cd9bf51ec0dbdc3a6adf
Imran Ariffin <ariffin.imran@gmail.com>
2022-11-20 23:31:13 -0500

    Run pylint separately for sample_apps; FAILED

--- lint.sh
+++ lint.sh
@@ -1,12 +1,15 @@
 if [ -z $1 ];
 then
     pylint -v --rcfile ./.pylintrc src/aiotaskq
-    failed_2=$?
-
-    pylint -v --rcfile ./.pylintrc.tests src/tests/
     failed_1=$?
 
-    [ $failed_1 -eq 0 ] && [ $failed_2 -eq 0 ] && failed=0 || failed=1
+    pylint -v --rcfile ./.pylintrc.tests src/tests/
+    failed_2=$?
+
+    pylint -v --rcfile ./.pylintrc.sample_apps src/sample_apps/
+    failed_3=$?
+
+    [ $failed_1 -eq 0 ] && [ $failed_2 -eq 0 ] && [ $failed_3 -eq 0 ] && failed=0 || failed=1
 else
     pylint -v $1
     failed=$?
