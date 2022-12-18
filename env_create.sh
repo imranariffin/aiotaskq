1b277c5217ffce6bc916a40dc4b965c976085a00
Imran Ariffin <ariffin.imran@gmail.com>
2022-12-18 17:43:49 -0500

    Fixup

--- env_create.sh
+++ env_create.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-py_version=$(python3 --version | grep -o 3.10)
+venv=${1:-.venv}
 
-if [ "$py_version" != "3.10" ]; then
+python3.10 -m venv $venv
+if [ "$?" != "0" ];
+then
     echo "Please install Python 3.10.x"
     exit 1
 fi
 
-venv=${1:-.venv}
-python3 -m venv $venv
 source $venv/bin/activate
-
-echo "Using $(python --version)"
-echo "Using $(pip --version)"
+echo "Using virtual environment $venv using $(python --version)"
+deactivate
