88cbca6f92962ddb7063b5cccc19ae22078cb27b
Imran Ariffin <ariffin.imran@gmail.com>
2022-12-18 13:49:18 -0500

    Fixup tests

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 echo "Enter virtual environment for testing"
 source ./env_activate.sh ./src/tests/.venv
 export LOG_LEVEL=${LOG_LEVEL:-INFO}
+python --version
 
 echo "Erase previous coverage files"
 coverage erase
@@ -8,7 +9,7 @@ coverage erase
 echo "Run tests"
 if [ -z $1 ];
 then
-    coverage run -m pytest -v
+    coverage run -m pytest -vvv -s
 else
     coverage run -m pytest -vvv -k $1 -s
 fi
