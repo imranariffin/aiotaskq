b5ca71626ad246503bbfe50b93a8b92151f3255c
Imran Ariffin <ariffin.imran@gmail.com>
2022-12-18 11:53:50 -0500

    Fixup

--- lint.sh
+++ lint.sh
@@ -1,11 +1,20 @@
 if [ -z $1 ];
 then
+    source ./enter_env.sh
+    echo "Installing dependencies"
+    source ./install_dependencies.sh > /dev/null
     pylint -v --rcfile ./.pylintrc src/aiotaskq
     failed_1=$?
 
+    source ./enter_env.sh ./src/tests/.venv/
+    echo "Installing dependencies"
+    source ./install_dependencies.sh ./src/tests/ > /dev/null
     pylint -v --rcfile ./.pylintrc.tests src/tests/
     failed_2=$?
 
+    source ./enter_env.sh ./src/sample_apps/.venv/
+    echo "Installing dependencies"
+    source ./install_dependencies.sh ./src/sample_apps/ > /dev/null
     pylint -v --rcfile ./.pylintrc.sample_apps src/sample_apps/
     failed_3=$?
 
--- src/tests/test_docs_sample_apps.py
+++ src/tests/test_docs_sample_apps.py
@@ -3,9 +3,11 @@ import subprocess
 
 def test_simple_app():
     proc_aiotaskq = subprocess.run(
-        ["sh", "./demo-sample-apps-simple-app-aiotaskq.sh"]
+        ["sh", "./demo-sample-apps-simple-app-aiotaskq.sh"],
+        check=False,
     )
     proc_celery = subprocess.run(
-        ["sh", "./demo-sample-apps-simple-app-celery.sh"]
+        ["sh", "./demo-sample-apps-simple-app-celery.sh"],
+        check=False,
     )
     assert proc_aiotaskq.returncode == proc_celery.returncode == 0
